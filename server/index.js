89cfffa2d430ba7e46683ee8d434b5e8639053eb
samarsinh <samarsinh225@gmail.com>
2021-12-01 08:32:49 +0530

    done

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+// import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
@@ -12,15 +13,19 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+
 app.get("/", (req, res) => {
   res.send("hello to justay api");
 });
-// const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
-
+const CONNECTION_URL =
+  "mongodb+srv://asdf1234:asdf1234@cluster0.d7feo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
